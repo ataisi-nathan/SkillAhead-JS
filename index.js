b7f37890d09e1d4e6da4f5e4e52b1c4d7c36707a
ataisinathan <ataisinathan@gmail.com>
2023-02-17 20:15:50 +0100

    new commit

--- index.js
+++ index.js
@@ -95,14 +95,14 @@
 // Conditional Statements
 
 // if else statement
-let link = "inactive";
+// let link = "inactive";
 
-if (link == "active") {
-    console.log("The link is active, start online class");
-}
-else {
-    console.log("The link is inactive, it seems there is no class today");
-}
+// if (link == "active") {
+//     console.log("The link is active, start online class");
+// }
+// else {
+//     console.log("The link is inactive, it seems there is no class today");
+// }
 
 // Exercise: Write a program that gets a user's input and checks if the number is either positive, negative, or zero.
 
@@ -139,29 +139,125 @@ else {
 
 
 // switch statement
-let time = prompt("");
-switch (time) {
-    case 0:
-        console.log("This is zero")
-        break;
-    case 1:
-        console.log("This is one")
-        break;
-    case 2:
-        console.log("This is two")
-        break;
-    case 3:
-        console.log("This is three")
-        break;
-    case 4:
-        console.log("This is four")
-        break;
-    case 5:
-        console.log("This is five")
-        break;
-    default:
-        console.log("Try again")
-        break;
-}
+// let time = prompt("");
+// switch (time) {
+//     case 0:
+//         console.log("This is zero")
+//         break;
+//     case 1:
+//         console.log("This is one")
+//         break;
+//     case 2:
+//         console.log("This is two")
+//         break;
+//     case 3:
+//         console.log("This is three")
+//         break;
+//     case 4:
+//         console.log("This is four")
+//         break;
+//     case 5:
+//         console.log("This is five")
+//         break;
+//     default:
+//         console.log("Try again")
+//         break;
+// }
 
 // Exercise: Create a program using the switch statement to get what day it is from Sunday to Saturday and print eg. "Today is Monday"
+
+
+// Functions: 
+
+// function skill(a, b) {
+//     return a + b;
+// }
+// console.log(skill(2,6));
+
+
+// Today's class: Arrays, Loops, and Objects
+
+// let student = ['Mary Sanni', 'David Obialor', 'Ataisi Nathan', "Temple Eze", "Precious Dev", "Grace-love"];
+// console.log(student);
+
+// student.push("Son Of Man");
+// console.log(student);
+
+// student.pop();
+// console.log(student);
+
+// student.splice(3, 2, "Bolaji", "Doris", "Promise");
+// console.log(student);
+
+// student.unshift("Bolaji");
+// console.log(student);
+
+// student.shift();
+// console.log(student);
+// console.log(typeof(student));
+
+// // student = student.join("\n");
+// // student = student.toString();
+// // console.log(student);
+
+// let fruits = ["apple", "Banana", "Carrot"];
+// let cars = ["Audi", "BMW", "Lexus"];
+// let country = ["Nigeria", "Ghana", "Ethopia"];
+
+// let joined = fruits.concat(cars, country, student);
+// console.log(joined);
+
+// for (let i = 0; i < joined.length; i++) {
+//     console.log(joined[i])
+// }
+
+// let person = { 
+//     head: "eye, nose, ears, mouth, tongue, teeth, hairs",
+//     body: "arms, novel, elbows, chest, breast",
+//     senses: "sight, smell, hearing, touch, taste"
+// }
+
+// console.log(person.head);
+// console.log(person.body);
+// console.log(person.senses);
+
+// DOM Manipulation: Document Object Model Manipulation.
+
+// Selecting Elements
+let h = document.getElementsByTagName('ul');
+console.log(h);
+
+let i = document.getElementById('demo');
+console.log(i);
+
+let j = document.getElementsByClassName('change');
+console.log(j);
+
+document.querySelector('#demo');
+
+let user = prompt("What is your name?");
+
+let a = document.querySelector('h1').innerText=user;
+console.log(a);
+
+let list = '<li>working</li><li>working</li><li>working</li><li>working</li>'
+
+document.querySelector('ul').innerHTML=list;
+
+document.querySelector('ul').setAttribute('style', 'font-size:2em;');
+
+let li = document.createElement('li');
+li = document.querySelector('ul').appendChild(li);
+let newWork = document.createTextNode('New Work');
+li.appendChild(newWork);
+
+let test = document.createElement('p');
+test = document.querySelector('body').appendChild(test);
+let Work = document.createTextNode('Changed');
+test.appendChild(Work);
+
+document.querySelector('#demo').remove();
+
+let item = document.getElementsByTagName('ul')[0];
+
+document.querySelector('body').replaceChild(test, item);
